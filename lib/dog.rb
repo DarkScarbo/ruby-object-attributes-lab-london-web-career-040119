0944d108bdac228397bd1915c22c7d63ab6288b4
Xabier Casán <xabier_abia@hotmail.com>
2019-04-02 19:51:09 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,20 @@
 class Dog
-  
-  def name=(new_name)
-    @name = new_name
+
+  def name=(name)
+    @name = name
   end
-  
-  
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+
 end
